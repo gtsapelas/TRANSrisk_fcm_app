e734961bbbee51f11bb64e3fc944e2e9937b1439
gtsapelas <gtsapelas@epu.ntua.gr>
2017-09-25 22:00:26 +0300

    added missing files

---
+++ fcm_app/forms.py
@@ -0,0 +1,8 @@
+from django import forms
+
+
+class FCMForm(forms.Form):
+    title = forms.CharField(label='Title', max_length=200, widget=forms.TextInput(attrs={'class': 'form-control'}))
+    description = forms.CharField(label='Description', widget=forms.Textarea(attrs={'class': 'form-control', 'rows': '4', 'cols': '50'}))
+    map_image = forms.FileField(label='Image', widget=forms.FileInput())
+    map_html = forms.FileField(label='HTML', widget=forms.FileInput)
