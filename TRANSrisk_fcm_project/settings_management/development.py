8f77ee81ce00bcc91072d9a07ab4e9d006e2b0a5
gtsapelas <gtsapelas@epu.ntua.gr>
2018-08-01 01:01:26 +0300

    settings

--- TRANSrisk_fcm_project/settings_management/development.py
+++ TRANSrisk_fcm_project/settings_management/development.py
@@ -3,13 +3,22 @@ from TRANSrisk_fcm_project.settings_management.base_settings import *
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 
+
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'transrisk_fcm_db',
+        'NAME': 'ddbso8btmk2j1i',
+        'USER': 'mxuhuoymiyjacp',
+        'PASSWORD': '1daacb5325597a7465bbd7a74cca1b0f175e73fc7da2f83d44a50e77a3a056de',
+        'HOST': 'ec2-54-227-251-233.compute-1.amazonaws.com',
+        'PORT': '5432',
     }
 }
 
-# No emails should be sent on development
-EMAIL_BACKEND = 'django.core.mail.backends.dummy.EmailBackend'
-CONTACT_EMAIL = 'myemail@gmail.com'
+
+MEDIA_URL = '/media/'
+
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+CKEDITOR_UPLOAD_PATH = 'uploads/'
+CKEDITOR_FILENAME_GENERATOR = 'utils.get_filename'
