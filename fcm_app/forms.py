3148f0142101a173b4aef1a8956c610e8e6731bf
dhmhtrhsv <dhmhtrhsv@hotmail.com>
2018-09-27 17:51:57 +0300

    Fix filtered_years and filtered_countries, fix text-overflow at my maps

--- fcm_app/forms.py
+++ fcm_app/forms.py
@@ -29,7 +29,6 @@ class FCMCONCEPTForm(forms.Form):
     concept_info = forms.CharField(widget=CKEditorWidget)
 
 def first_year():
-    return 2017
     return int(FCM.objects.all().order_by('creation_date')[0].creation_date.strftime('%Y'))
 
 def last_year():
@@ -37,23 +36,55 @@ def last_year():
     now = datetime.datetime.now()
     return now.year
 
+def selected_years():
+    x = FCM.objects.all()
+    YEAR_CHOICES = []
+    if x!=0:
+        # YEAR_CHOICES = [(str(i), i) for i in range(first_year(), last_year() + 1)]
+        YEAR_CHOICES = [(str(i.year),i.year) for i in FCM.objects.all().datetimes('creation_date', 'year')]
+    YEAR_CHOICES.insert(0, ('-', '---'))
+    return YEAR_CHOICES
+
 MONTHS_CHOICES = [(str(i), calendar.month_name[i]) for i in range(1,13)]
 MONTHS_CHOICES.insert(0,('-','---'))
 
-YEAR_CHOICES = [(str(i), i) for i in range(first_year(),last_year()+1)]
-YEAR_CHOICES.insert(0,('-','---'))
+# COUNTRIES_CHOICES = [(countries[i][0], countries[i][1]) for i in range(0,len(countries))]
+# COUNTRIES_CHOICES.insert(0,('-','---'))
 
-COUNTRIES_CHOICES = [(countries[i][0], countries[i][1]) for i in range(0,len(countries))]
-COUNTRIES_CHOICES.insert(0,('-','---'))
+
+def selected_countries():
+    x = FCM.objects.all().distinct('country')
+    if x!=0:
+        my = x.values('country')
+        my_countries_codes = []
+        for index in range(len(my)):
+            for key in my[index]:
+                my_countries_codes.append(my[index][key])
+        oles = [(countries[i][0], countries[i][1]) for i in range(0,len(countries))]
+        final_countries = []
+        for m in my_countries_codes:
+            final_countries.append([item for item in oles if item[0] == m])
+        flat_list = [item for sublist in final_countries for item in sublist]
+    flat_list.insert(0, ('-', '---'))
+    return flat_list
+
 
 class FiltersForm(forms.Form):
+
+    def __init__(self, *args, **kwargs):
+        # super(self.__class__, self).__init__(*args, **kwargs)
+        super(FiltersForm, self).__init__(*args, **kwargs)
+        self.fields['filtered_year'].choices = selected_years()
+        self.fields['filtered_country'].choices = selected_countries()
+
     filtered_title_and_or_description = forms.CharField(label='Filter fcms which contain the word...', required=False, widget=forms.TextInput(attrs={'class': 'searchbox-input form-control'}))
     filtered_tags = MyMultipleChoiceField(label='Filter fcms which contain one of the tags...', required=False,
                                               widget=forms.SelectMultiple(attrs={'multiple': 'multiple'}),
                                               choices=())
-    filtered_year = forms.CharField(widget=forms.Select(choices=YEAR_CHOICES))
-    filtered_country = forms.CharField(widget=forms.Select(choices=COUNTRIES_CHOICES))
     filtered_getmine = forms.BooleanField(required=False)
+    filtered_year = forms.ChoiceField(widget=forms.Select(choices=[]))
+    filtered_country = forms.ChoiceField(widget=forms.Select(choices=[]))
+
 
 class jsForm(forms.Form):
     title = forms.CharField(label='Title', max_length=200, widget=forms.TextInput(attrs={'class': 'form-control', 'autocomplete': 'off'}))
